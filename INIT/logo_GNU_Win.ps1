4ec653ed19a5556c480094aca66066f2d3f54fc7
n132 <986256128@qq.com>
2021-09-15 20:45:25 -0400

    some interesting sh

---
+++ INIT/logo_GNU_Win.ps1
@@ -0,0 +1,29 @@
+Write-Color -Text ""         -Color Cyan
+Write-Color -Text "     ----:++oso/:-                .--:/-:---.     "         -Color Blue
+Write-Color -Text "  ./:/+dyy+o+/+//-/+-           +:.:/o+soyys::/`  "         -Color Blue
+Write-Color -Text " ::.hMo/:-.     ``               ````   .--+dm::- "         -Color Blue
+Write-Color -Text ":.`hMo:-        .---:/:--. .---:-..`       ::dm---"         -Color Blue
+Write-Color -Text "/ :Mm:-       .:-+dNMMMNy:/ssomNMdo--`      :+Mo /"         -Color Blue
+Write-Color -Text "/ /Mm/`     `/-sMMMMMMMMMM` -..hMMMMs--     //Ms /"         -Color Blue
+Write-Color -Text "/ :NN:/   `/:+Nmdhs+/oo/.`:.`---:doymm/-.  `:yM/ +"         -Color Cyan
+Write-Color -Text "/``yMd/:-:osd+.   -/hh   -/ .+-:o-:  :mds/-/yNm.`/"         -Color Cyan
+Write-Color -Text " /`:dMMMhNMs     -/ /``.-- :   d:`/   `hMMNMMd/./ "         -Color Cyan
+Write-Color -Text "  ::`:ydmNo       .  ++mm: ``  +hMs`   .mmyo:.:-  "         -Color Red
+Write-Color -Text "   `:::--y       `/  `.--..     -s+`    `o/---    "         -Color Red
+Write-Color -Text "       `/:      .s:              `+o--.   /:      "         -Color Red
+Write-Color -Text "       .o`    ./: .      `++:-:-  `o:-:-----      "         -Color Red
+Write-Color -Text "      //`.+///y.      `  `y`s+- -.`   --          "         -Color Red
+Write-Color -Text "     `+:/:o-  /:     . ```  .`..     `+.          "         -Color Cyan
+Write-Color -Text "          o-  `y`       -/        `-o/            "         -Color Cyan
+Write-Color -Text "          //   -/+`      -/`     `...s`           "         -Color Cyan
+Write-Color -Text "           y     :y        -:::::-:/o`            "         -Color Cyan
+Write-Color -Text "           .o    `/os.      `-:/+/:/.             "         -Color Cyan
+Write-Color -Text "            `/.     s/+o`   `:-.y                 "         -Color Green
+Write-Color -Text "              .-`   +sNmd     - +.                "         -Color Green
+Write-Color -Text "                     `ooom+`+hmo.                 "         -Color Green
+Write-Color -Text "                     -. odNyMMMNs/.               "         -Color Cyan
+Write-Color -Text "                        .`odddyhh/     "         -Color Cyan
+Write-Color -Text " "
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt Paradox
